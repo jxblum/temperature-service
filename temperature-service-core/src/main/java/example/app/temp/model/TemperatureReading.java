c1d363b8b9f387be97893a4e41907492659bc999
John Blum <jblum@pivotal.io>
2018-10-09 17:15:26 -0700

    Add TemperatureReading application domain model type to the example.app.temp.model package to model Temperature readings recorded by a temperature sensor and acted on by a temperature monitor.

---
+++ temperature-service-core/src/main/java/example/app/temp/model/TemperatureReading.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package example.app.temp.model;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.gemfire.mapping.annotation.Region;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * The {@link TemperatureReading} class is an Abstract Data Type (ADT) modeling a temperature reading,
+ * which may have originated from a sensor or thermometer.
+ *
+ * @author John Blum
+ * @see java.util.UUID
+ * @see lombok
+ * @see org.springframework.data.gemfire.mapping.annotation.Region
+ * @since 1.0.0
+ */
+@Data
+@Region("TemperatureReadings")
+@RequiredArgsConstructor(staticName = "of")
+@SuppressWarnings("unused")
+public class TemperatureReading {
+
+	@Id
+	private String id = UUID.randomUUID().toString();
+
+	private Long timestamp = System.currentTimeMillis();
+
+	@NonNull
+	private final Temperature temperature;
+
+}
