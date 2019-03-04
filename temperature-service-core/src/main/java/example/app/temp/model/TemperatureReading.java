a8d329d0de1b34f941e16835c27eb9403f0097cb
John Blum <jblum@pivotal.io>
2019-03-04 09:55:46 -0800

    Optimize imports.

--- temperature-service-core/src/main/java/example/app/temp/model/TemperatureReading.java
+++ temperature-service-core/src/main/java/example/app/temp/model/TemperatureReading.java
@@ -17,12 +17,12 @@ package example.app.temp.model;
 
 import java.util.UUID;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.gemfire.mapping.annotation.Region;
-
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.gemfire.mapping.annotation.Region;
 
 /**
  * The {@link TemperatureReading} class is an Abstract Data Type (ADT) modeling a temperature reading,
