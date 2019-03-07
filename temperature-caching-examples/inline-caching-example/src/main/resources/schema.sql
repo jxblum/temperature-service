1312791d1389a1a24c9dd9a1f72c35fe49b7ddc5
John Blum <jblum@pivotal.io>
2019-03-07 10:54:06 -0800

    Rename tables using lowercase letters and underscores.
    Set size limit on VARCHAR fields.
    
    Add Foreign Key from temperature_readings.temp_sensor_id to temperature_sensors.id.

--- temperature-caching-examples/inline-caching-example/src/main/resources/schema.sql
+++ temperature-caching-examples/inline-caching-example/src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS TemperatureSensors (
+CREATE TABLE IF NOT EXISTS temperature_sensors (
   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1),
   location OTHER,
-  name VARCHAR NOT NULL
+  name VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS TemperatureReadings (
-  id VARCHAR PRIMARY KEY DEFAULT TO_CHAR(CURRENT_TIMESTAMP),
+CREATE TABLE IF NOT EXISTS temperature_readings (
+  id VARCHAR(256) PRIMARY KEY,
   temp_sensor_id BIGINT,
   date_time BIGINT,
   measurement DECIMAL(10, 2),
   scale VARCHAR(10),
-  FOREIGN KEY (temp_sensor_id) REFERENCES TemperatureSensors(id)
+  FOREIGN KEY (temp_sensor_id) REFERENCES temperature_sensors(id)
 );
