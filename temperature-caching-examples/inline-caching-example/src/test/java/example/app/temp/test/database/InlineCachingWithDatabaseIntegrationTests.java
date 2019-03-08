c663bab54db3e437d8ca27e259e730fe1e0ebf5d
John Blum <jblum@pivotal.io>
2019-03-07 16:36:54 -0800

    Refactor InlineCachingWithDatabaseIntegrationTests to run inline caching test cases (read-through, write-behind) based on a temperature sensor device management service.

--- temperature-caching-examples/inline-caching-example/src/test/java/example/app/temp/test/database/InlineCachingWithDatabaseIntegrationTests.java
+++ temperature-caching-examples/inline-caching-example/src/test/java/example/app/temp/test/database/InlineCachingWithDatabaseIntegrationTests.java
@@ -17,16 +17,21 @@ package example.app.temp.test.database;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.apache.geode.cache.GemFireCache;
+import org.cp.elements.util.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.gemfire.GemfireTemplate;
 import org.springframework.data.gemfire.LocalRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.PeerCacheApplication;
@@ -40,12 +45,27 @@ import example.app.temp.model.Temperature;
 import example.app.temp.model.TemperatureReading;
 import example.app.temp.model.TemperatureSensor;
 import example.app.temp.model.Temperatures;
-import example.app.temp.repo.TemperatureReadingRepository;
+import example.app.temp.repo.TemperatureSensorRepository;
+import example.app.temp.util.TemperatureReadingComparator;
 
 /**
- * The InlineCachingWithDatabaseIntegrationTests class...
+ * Integration tests for inline caching using Apache Geode / Pivotal GemFire with an RDBMS
+ * along with Apache Cassandra.
  *
  * @author John Blum
+ * @see org.junit.Test
+ * @see org.apache.geode.cache.GemFireCache
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.test.context.SpringBootTest
+ * @see org.springframework.data.gemfire.config.annotation.PeerCacheApplication
+ * @see org.springframework.data.jpa.repository.config.EnableJpaRepositories
+ * @see org.springframework.test.context.junit4.SpringRunner
+ * @see example.app.temp.geode.cache.RepositoryCacheLoader
+ * @see example.app.temp.geode.cache.RepositoryCacheWriter
+ * @see example.app.temp.model.Temperature
+ * @see example.app.temp.model.TemperatureReading
+ * @see example.app.temp.model.TemperatureSensor
+ * @see example.app.temp.repo.TemperatureSensorRepository
  * @since 1.0.0
  */
 @RunWith(SpringRunner.class)
@@ -56,38 +76,69 @@ public class InlineCachingWithDatabaseIntegrationTests {
 
   private static final String GEMFIRE_LOG_LEVEL = "warn";
 
-  private static final TemperatureReading ROOM_TEMPERATURE_READING =
-    TemperatureReading.of(Temperatures.ROOM_TEMPERATURE_FAHRENHEIT.getTemperature());
+  @Autowired
+  private GemfireTemplate temperatureSensorsTemplate;
+
+  private volatile TemperatureSensor sensorA;
 
   @Autowired
-  private GemfireTemplate temperatureReadingsTemplate;
+  private TemperatureSensorRepository repository;
 
-  @Autowired
-  private TemperatureReadingRepository repository;
+  protected void assertEquals(TemperatureSensor actualSensor, TemperatureSensor expectedSensor) {
+
+    assertThat(actualSensor).isNotNull();
+    assertThat(actualSensor.getId()).isEqualTo(expectedSensor.getId());
+    assertThat(actualSensor.getLocation()).isEqualTo(expectedSensor.getLocation());
+    assertThat(actualSensor.getName()).isEqualTo(expectedSensor.getName());
+
+    List<TemperatureReading> actualTemperatureReadings =
+      CollectionUtils.nullSafeCollection(actualSensor.getTemperatureReadings()).stream()
+        .sorted(TemperatureReadingComparator.ORDER_BY_TIME_TEMP_ASC)
+        .collect(Collectors.toList());
+
+    List<TemperatureReading> expectedTemperatureReadings =
+      CollectionUtils.nullSafeCollection(expectedSensor.getTemperatureReadings()).stream()
+        .sorted(TemperatureReadingComparator.ORDER_BY_TIME_TEMP_ASC)
+        .collect(Collectors.toList());
+
+    assertThat(actualTemperatureReadings).hasSize(expectedTemperatureReadings.size());
+
+    for (int index = expectedTemperatureReadings.size(); --index >= 0; ) {
+      assertThat(actualTemperatureReadings.get(index)).isEqualTo(expectedTemperatureReadings.get(index));
+    }
+  }
 
   @Before
   public void setup() {
 
-    assertThat(this.repository.count()).isEqualTo(0);
-    assertThat(this.temperatureReadingsTemplate.getRegion()).hasSize(0);
+    if (this.sensorA == null) {
 
-    this.repository.save(ROOM_TEMPERATURE_READING);
+      this.sensorA = this.repository.findByName("A");
 
-    assertThat(this.repository.count()).isEqualTo(1);
-    assertThat(this.repository.existsById(ROOM_TEMPERATURE_READING.getId())).isTrue();
-    assertThat(this.repository.findById(ROOM_TEMPERATURE_READING.getId()).orElse(null)).isEqualTo(
-      ROOM_TEMPERATURE_READING);
-    assertThat(this.temperatureReadingsTemplate.<String, TemperatureReading>get(ROOM_TEMPERATURE_READING.getId())).isNull();
-    assertThat(this.temperatureReadingsTemplate.containsKey(ROOM_TEMPERATURE_READING.getId())).isFalse();
-    assertThat(this.temperatureReadingsTemplate.getRegion()).hasSize(0);
+      assertThat(this.sensorA).isNotNull();
+      assertThat(this.sensorA.getId()).isNotNull();
+      assertThat(this.sensorA.getLocation()).isNull();
+      assertThat(this.sensorA.getName()).isEqualTo("A");
+
+      List<Temperature> sensorTemperatureReadings =
+        CollectionUtils.nullSafeCollection(this.sensorA.getTemperatureReadings()).stream()
+          .sorted(TemperatureReadingComparator.ORDER_BY_TIME_TEMP_ASC)
+          .map(TemperatureReading::getTemperature)
+          .collect(Collectors.toList());
+
+      assertThat(sensorTemperatureReadings).hasSize(2);
+      assertThat(sensorTemperatureReadings).contains(
+        Temperatures.ROOM_TEMPERATURE_FAHRENHEIT.getTemperature(),
+        Temperature.of(0.0d).celsius()
+      );
+    }
   }
 
   @Test
   public void cacheLoadsFromDatabase() {
 
-    assertThat(this.temperatureReadingsTemplate.containsKey(ROOM_TEMPERATURE_READING.getId())).isFalse();
-    assertThat(this.temperatureReadingsTemplate.<String, TemperatureReading>get(ROOM_TEMPERATURE_READING.getId()))
-      .isEqualTo(ROOM_TEMPERATURE_READING);
+    assertThat(this.temperatureSensorsTemplate.containsKey(sensorA.getId())).isFalse();
+    assertEquals(this.temperatureSensorsTemplate.get(sensorA.getId()), sensorA);
   }
 
   @Test
@@ -95,41 +146,54 @@ public class InlineCachingWithDatabaseIntegrationTests {
 
     TemperatureReading nineElevenDegreesFahrenheit = TemperatureReading.of(Temperature.of(911).fahrenheit());
 
-    assertThat(this.temperatureReadingsTemplate.containsKey(nineElevenDegreesFahrenheit.getId())).isFalse();
-    assertThat(this.repository.existsById(nineElevenDegreesFahrenheit.getId())).isFalse();
+    TemperatureSensor localSensor = TemperatureSensor.create("B")
+      .add(nineElevenDegreesFahrenheit)
+      .identifiedBy(this.sensorA.getId() + 1L);
 
-    this.temperatureReadingsTemplate.put(nineElevenDegreesFahrenheit.getId(), nineElevenDegreesFahrenheit);
+    assertThat(this.temperatureSensorsTemplate.containsKey(localSensor.getId())).isFalse();
+    assertThat(this.repository.existsById(localSensor.getId())).isFalse();
 
-    assertThat(this.temperatureReadingsTemplate.<String, TemperatureReading>get(nineElevenDegreesFahrenheit.getId()))
-      .isEqualTo(nineElevenDegreesFahrenheit);
-    assertThat(this.repository.findById(nineElevenDegreesFahrenheit.getId()).orElse(null))
-      .isEqualTo(nineElevenDegreesFahrenheit);
+    this.temperatureSensorsTemplate.put(localSensor.getId(), localSensor);
+
+    assertThat(this.temperatureSensorsTemplate.<Long, TemperatureSensor>get(localSensor.getId())).isEqualTo(localSensor);
+    assertEquals(this.repository.findById(localSensor.getId()).orElse(null), localSensor);
   }
 
-  @SpringBootApplication
   @PeerCacheApplication(logLevel = GEMFIRE_LOG_LEVEL)
-  @EntityScan(basePackageClasses = TemperatureSensor.class)
-  @EnableJpaRepositories(basePackageClasses = TemperatureReadingRepository.class)
   static class GeodeConfiguration {
 
-    @Bean("TemperatureReadings")
-    public LocalRegionFactoryBean<String, TemperatureReading> temperatureReadingsRegion(GemFireCache gemfireCache,
-        TemperatureReadingRepository repository) {
+    @Bean("TemperatureSensors")
+    public LocalRegionFactoryBean<Long, TemperatureSensor> temperatureSensorsRegion(GemFireCache gemfireCache,
+        TemperatureSensorRepository repository) {
 
-      LocalRegionFactoryBean<String, TemperatureReading> localRegion = new LocalRegionFactoryBean<>();
+      LocalRegionFactoryBean<Long, TemperatureSensor> temperatureSensorsRegion = new LocalRegionFactoryBean<>();
 
-      localRegion.setCache(gemfireCache);
-      localRegion.setCacheLoader(new RepositoryCacheLoader<>(repository));
-      localRegion.setCacheWriter(new RepositoryCacheWriter<>(repository));
-      localRegion.setClose(false);
-      localRegion.setPersistent(false);
+      temperatureSensorsRegion.setCache(gemfireCache);
+      temperatureSensorsRegion.setCacheLoader(new RepositoryCacheLoader<>(repository));
+      temperatureSensorsRegion.setCacheWriter(new RepositoryCacheWriter<>(repository));
+      temperatureSensorsRegion.setClose(false);
+      temperatureSensorsRegion.setPersistent(false);
 
-      return localRegion;
+      return temperatureSensorsRegion;
     }
 
     @Bean
-    public GemfireTemplate temperatureReadingsTemplate(GemFireCache gemfireCache) {
-      return new GemfireTemplate(gemfireCache.getRegion("/TemperatureReadings"));
+    public GemfireTemplate temperatureSensorsTemplate(GemFireCache gemfireCache) {
+      return new GemfireTemplate(gemfireCache.getRegion("/TemperatureSensors"));
     }
   }
+
+  @Profile("database")
+  @SpringBootApplication
+  @EntityScan(basePackageClasses = TemperatureSensor.class)
+  @EnableJpaRepositories(basePackageClasses = TemperatureSensorRepository.class)
+  static class DatabaseConfiguration { }
+
+  /*
+  @Profile("cassandra")
+  @SpringBootApplication
+  @EnableCassandraRepositories(basePackageClasses = TemperatureSensorRepository.class)
+  static class CassandraConfiguration { }
+  */
+
 }
